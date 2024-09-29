2a40bab1f98e63f4e1e32886ff5d3dd088be6630
DillyTorgs <Djt5832@psu.edu>
2024-09-29 16:10:41 -0400

    now

--- src/my-card.js
+++ src/my-card.js
@@ -14,24 +14,103 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.title = "My card";
+    this.title = '';
+    this.link = '#';
+    this.image = null;
+    this.description = '';
+    this.buttonDesc = '';
+    this.fancy = false;
+
+
+    
   }
 
   static get styles() {
     return css`
       :host {
-        display: block;
+        display: inline-block;
+      }
+      :host([fancy]) {
+       display: block;
+       background-color: pink;
+       border: 2px solid fuchsia;
+      box-shadow: 10px 5px 5px red;
+}
+
+      
+      .card {
+        padding: 4px;
+        width: 275px;
+        border-radius: 8px;
+        text-align: center;
+        background-color: white;
+        border: 2px groove;
+      }
+ 
+      .cardTitle {
+        margin: 16px;
+        font-size: 24px;
+      }
+
+      img {
+        margin: auto;
+        display: flex;
+        height: 150px;
+        width: 270px;
+      }
+
+      button {
+        margin: auto;
+        display: flex;
+        background-color: pink;
+      }
+
+      a:focus,
+      a:hover {
+        color: #202A44;;
+      }
+
+      a {
+        text-decoration: none;
+        color: white;
       }
     `;
   }
+  // put this anywhere on the MyCard class; just above render() is probably good
+   openChanged(e) {
+    console.log(e.newState);
+    if (e.newState === "open") {
+      this.fancy = true;
+    }
+    else {
+      this.fancy = false;
+    }
+  }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+    <div class="card">
+      <img src="${this.image}">
+      <div class="cardTitle">${this.title}</div>
+      <p>${this.description}</p>
+      <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+        <summary>Description</summary>
+        <div>
+        <slot name="description">${this.description}</slot>
+      </div>
+    </details>
+      <button><a href="${this.link}">${this.buttonDesc}</a></button>
+    </div>`;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      image: { type: String},
+      link: { type: String},
+      description: { type: String},
+      buttonDesc: { type: String},
+      fancy: { type: Boolean, reflect: true },
     };
   }
 }
